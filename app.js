902afad1f61c9b7f745dd5d37bcf371524ae949a
ShamsutOFF <shamsutoff@yandex.ru>
2025-12-24 08:34:07 +0300

    Arrays fix

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 function crypto(password) {
-    let result = '';
+    const arr = password.split('');
+    const result = [];
 
-    for (let i = 0; i < password.length; i += 2) {
-        if (i + 1 < password.length) {
-            result += password[i + 1] + password[i];
+    for (let i = 0; i < arr.length; i += 4) {
+        const group = arr.slice(i, i + 4);
+        if (group.length === 4) {
+            // console.log(group)
+            if (i === 4) {
+                // console.log(i)
+                // console.log(group)
+                // console.log(group[3], group[1], group[2], group[0])
+                result.push(group[3], group[1], group[2], group[0]);
+            } else {
+                result.push(group.reverse().join(''));
+            }
+
         } else {
-            result += password[i];
+            result.push(...group);
         }
     }
 
-    return result;
+    return result.join('');
 }
 
 function check(encrypted, password) {
     return crypto(password) === encrypted;
 }
 
-// Примеры
-console.log(crypto('password'));
-console.log(crypto('test'));
-console.log(check('etts', 'test'));
-console.log(check('etts', 'wrong'));
-console.log(check(crypto('password'), 'password'));
-console.log(check(crypto('password'), 'password2'));
+// Тесты
+console.log(crypto('password')); // 'ssapdorw'
+console.log(check('ssapdorw', 'password')); // true
+console.log(check(crypto('password'), 'password')); // true
+console.log(check(crypto('ssapdorw'), 'ssapdorw')); // true
+console.log(check('ssapdorw', 'wrong')); // false
