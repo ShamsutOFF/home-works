6cd6ac2377e2422f4219271d21f61aec53361888
ShamsutOFF <shamsutoff@yandex.ru>
2025-12-29 17:46:09 +0300

    OnClick

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 `use strict`;
+
+// document.querySelector(".button").addEventListener(
+//     "click", () => {
+//         const input = document.querySelector(".input").value;
+//         if (!input) {
+//             return
+//         }
+//         document.querySelector(".panel").innerHTML = input;
+//         document.querySelector(".input").value = "";
+//         console.log(input)
+//     });
+
+function changeClick() {
+    const input = document.querySelector(".input").value;
+    if (!input) {
+        return
+    }
+    document.querySelector(".panel").innerHTML = input;
+    document.querySelector(".input").value = "";
+    console.log(input)
+}
